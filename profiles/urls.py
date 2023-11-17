c1a11a267fd030c7f64a1bbcd1548392245a5260
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-17 01:29:29 +0100

    profiles app urls

---
+++ profiles/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+app_name = "profiles"
+
+
+urlpatterns = [
+    path('profiles/', views.profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', views.profile, name='profile'),
+    # path('admin/', admin.site.urls),
+]
