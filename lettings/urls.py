b3c761909bc2096acf513d85d6249ed131353918
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-17 01:00:54 +0100

    lettings app urls

--- lettings/urls.py
+++ lettings/urls.py
@@ -4,3 +4,10 @@ from django.urls import path
 from . import views
 
 app_name = "lettings"
+
+
+urlpatterns = [
+    path('lettings/', views.lettings_index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', views.letting, name='letting'),
+    # path('admin/', admin.site.urls),
+]
