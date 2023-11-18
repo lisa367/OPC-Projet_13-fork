c32d59088f8d74758f5ee4e4c6ac3ba35bbe9dfe
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-18 04:44:15 +0100

    apps tests files

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+# from django.test import TestCase
+import pytest
+from django.test import Client
+from .models import Address, Letting
 
 # Create your tests here.
+@pytest.mark.django_db  
+def test_address_model():
+    client = Client()
+    address = Address.objects.create(
+               author = "Jules Verne",
+               title = "20 milles lieues sous les mers")
+    expected_value = "Jules Verne | 20 milles lieues sous les mers"
+    assert str(address) == expected_value
+
+
+@pytest.mark.django_db 
+def test_letting_model():
+    pass
+
+
+def test_letting_index_view():
+    pass
+
+
+def test_letting_view():
+    pass
+
+
+def test_letting_index_url():
+    pass
+
+
+def test_letting_url():
+    pass
+""" 
+@pytest.mark.django_db  
+def test_book_model():
+    client = Client()
+    book = Book.objects.create(
+               author = "Jules Verne",
+               title = "20 milles lieues sous les mers")
+    expected_value = "Jules Verne | 20 milles lieues sous les mers"
+    assert str(book) == expected_value
+"""
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+# from django.test import TestCase
+import pytest
+from django.test import Client
+from .models import Profile
 
 # Create your tests here.
+@pytest.mark.django_db 
+def test_profile_model():
+    pass
+
+def test_profile_index_view():
+    pass
+
+
+def test_profile_view():
+    pass
+
+
+def test_profile_index_url():
+    pass
+
+
+def test_profile_url():
+    pass
