b7d6a53a98c004c01990a41087aefebb1076a479
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-18 01:52:46 +0100

    lettings Adress model - verbose_name_plural

--- lettings/models.py
+++ lettings/models.py
@@ -13,6 +13,9 @@ class Address(models.Model):
 
     def __str__(self):
         return f'{self.number} {self.street}'
+    
+    class Meta:
+        verbose_name_plural = "Adresses"
 
 
 class Letting(models.Model):
