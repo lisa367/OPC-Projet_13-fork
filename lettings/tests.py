f240f7336ddf2efe8cd1f1486cbf94cf460bbaed
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-18 15:38:47 +0100

    updated apss tests - models testing functions

--- lettings/tests.py
+++ lettings/tests.py
@@ -8,15 +8,24 @@ from .models import Address, Letting
 def test_address_model():
     client = Client()
     address = Address.objects.create(
-               author = "Jules Verne",
-               title = "20 milles lieues sous les mers")
-    expected_value = "Jules Verne | 20 milles lieues sous les mers"
+            number = 55,
+            street = "rue du Fbg Saint-Honoré",
+            city = "Paris",
+            state = "FRANCE",
+            zip_code = 75008,
+            country_iso_code = "FR")
+    expected_value = "55 rue du Fbg Saint-Honoré"
     assert str(address) == expected_value
 
 
 @pytest.mark.django_db 
 def test_letting_model():
-    pass
+    client = Client()
+    address = Letting.objects.create(
+            title = "Luxury private hotel in Paris",
+            address = 1)
+    expected_value = "Luxury private hotel in Paris"
+    assert str(address) == expected_value
 
 
 def test_letting_index_view():
--- profiles/tests.py
+++ profiles/tests.py
@@ -2,11 +2,16 @@
 import pytest
 from django.test import Client
 from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
 @pytest.mark.django_db 
 def test_profile_model():
-    pass
+    client = Client()
+    user = User.objects.create(username = "test_user", email="test_user@fauxmail.com" )
+    profile = Profile.objects.create(user=user.pk, favorite_city="Paris")
+    expected_value = "test_user"
+    assert str(profile) == expected_value
 
 def test_profile_index_view():
     pass
