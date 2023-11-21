bcb781e6fd77848f1c51787700db549ea1c870ef
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-11-21 02:10:05 +0100

    included apps urls in the root urls file

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,9 +5,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('lettings/', include("lettings.urls")),
+    path('profiles/', include("profiles.urls")),
+    path('admin/', admin.site.urls),
+]
+
+""" 
+urlpatterns = [
+    path('', views.index, name='index'),
     path('lettings/', views.lettings_index, name='lettings_index'),
     path('lettings/<int:letting_id>/', views.letting, name='letting'),
     path('profiles/', views.profiles_index, name='profiles_index'),
     path('profiles/<str:username>/', views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
+"""
