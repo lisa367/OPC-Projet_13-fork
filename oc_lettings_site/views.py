fb7e49d81b0cbbc2731463775727494d6b094678
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-02 00:13:21 +0100

    oc_lettings_site - added docstrings to the index view

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
+    """View to access the landing page of the web app
+
+    Args:
+        request (request): Request passed from the url
+
+    Returns:
+        _type_: Returns the template that displays the homepage of the web app
+    """
     return render(request, 'index.html')
