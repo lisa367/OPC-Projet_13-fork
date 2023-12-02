8fe5e3284b09da217d1d2e6660585b27f69f3e28
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-03 00:01:47 +0100

    profiles app - added docstrings to profile index view

--- profiles/views.py
+++ profiles/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render, get_object_or_404
 from .models import Profile
 
 
-# Create your views here.
-# Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero pulvinar eget. Fusc
-# faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum lacus d
 def profiles_index(request):
+    """View to display the list of all the profiles in the database
+
+    Args:
+        request (request): _description_
+
+    Returns:
+        _type_: _description_
+    """
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
     return render(request, 'profiles/index.html', context)
 
-# Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
-# laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt, dolor id facilisis fringilla, eros leo tristique lacus,
-# it. Nam aliquam dignissim congue. Pellentesque habitant morbi tristique senectus et netus et males
+
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
