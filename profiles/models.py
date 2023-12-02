6e911a9ec824507631b50ec0e8fa7f9327c21fa2
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-02 01:14:37 +0100

    profiles app models - added docstrings to the Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -2,8 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Profile(models.Model):
+    """
+    Class to create a profile object in the database
+
+    ...
+
+    Attributes
+    ----------
+    user : int
+        foreign key of the user related to the profile
+    favorite_city : str
+        name of the favorite city of the profile
+
+    Methods
+    -------
+    __str__:
+        Prints the string representation of an profile object
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     favorite_city = models.CharField(max_length=64, blank=True)
 
