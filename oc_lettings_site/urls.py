9d0a7db0e78b24c93d0f428d0fa0cc6602175664
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-05 22:34:50 +0100

    oc_letting_site app - updated files

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,6 +4,9 @@ from django.urls import path, include
 from . import views
 
 
+# def trigger_error(request):
+#   division_by_zero = 1 / 0
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include("lettings.urls")),
@@ -12,16 +15,3 @@ urlpatterns = [
     # path('sentry-debug/', trigger_error),
 ]
 
-""" 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
-]
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
-"""
