47845167cafa4d9ccefb9a5c65de92d67b8579eb
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-18 18:47:23 +0100

    settings.py - updated ALLOWED_HOSTS variables

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ["*"]
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 DEBUG = os.environ.get('DEBUG', True)
 DSN = os.environ.get('DSN')
-# ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
