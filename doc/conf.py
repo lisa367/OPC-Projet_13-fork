53ccfc5f7bf74d8f391c1936c69dbce9b48f62e4
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-28 18:30:15 +0100

    updated doc conf to exclude migrations submodules

--- doc/conf.py
+++ doc/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+# sys.path.insert(0, os.path.abspath(".."))
 
 project = "OC_Lettings"
 copyright = "2023, Lisa L."
@@ -17,11 +17,12 @@ author = "Lisa L."
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
+""" extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
-]
+] """
 
+extensions = []
 
 templates_path = ["_templates"]
 exclude_patterns = [
@@ -32,8 +33,11 @@ exclude_patterns = [
     ".env",
     ".circleci",
     ".idea",
-    "**/migrations",
-    "**/manage.py",
+    "lettings/migrations/*",
+    "profiles/migrations/*",
+    "oc_lettings_site/migrations/*",
+    "**/migrations/*",
+    "./manage.py",
     "manage.py",
 ]
 
