9cbac8b45cbc04c484861c9aa45e6fe0bc15a26d
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-12-30 09:59:07 +0100

    added static to urlpatterns

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -13,4 +15,4 @@ urlpatterns = [
     path('profiles/', include("profiles.urls")),
     path('admin/', admin.site.urls),
     # path('sentry-debug/', trigger_error),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
